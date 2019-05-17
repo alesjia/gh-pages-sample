b4b34cd35b1d1167018ac5711001d5080419e804
alesjia <alesjia@163.com>
2019-05-17 14:40:42 +0800

    Updates

--- precache-manifest.4e96d40311e722bb5f0ec98194d307c2.js
+++ precache-manifest.4e96d40311e722bb5f0ec98194d307c2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c2f64381432767c2f22ec31c68293805",
+    "revision": "4a0181f2e12b672da8fbedb4d6e3c7bf",
     "url": "/gh-pages-sample/index.html"
   },
   {
-    "revision": "b830140ba08c6850efcc",
+    "revision": "deb80d8a7bc20f503f53",
     "url": "/gh-pages-sample/static/css/main.2cce8147.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/gh-pages-sample/static/js/2.b41502e9.chunk.js"
   },
   {
-    "revision": "b830140ba08c6850efcc",
-    "url": "/gh-pages-sample/static/js/main.65bd29eb.chunk.js"
+    "revision": "deb80d8a7bc20f503f53",
+    "url": "/gh-pages-sample/static/js/main.8e1d121e.chunk.js"
   },
   {
     "revision": "a7a21f6213295ee03c3d",
