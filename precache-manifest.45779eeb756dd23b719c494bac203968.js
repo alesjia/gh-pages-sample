23a4dab526bc704543bcaf52db10e5c9559c768c
alesjia <alesjia@163.com>
2019-05-17 14:28:43 +0800

    Updates

---
+++ precache-manifest.45779eeb756dd23b719c494bac203968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba91783215fd771619ef82e55ef7f06f",
+    "url": "/alesjia/gh-pages-sample/index.html"
+  },
+  {
+    "revision": "c402aa483b2fa13a2d26",
+    "url": "/alesjia/gh-pages-sample/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/alesjia/gh-pages-sample/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c402aa483b2fa13a2d26",
+    "url": "/alesjia/gh-pages-sample/static/js/main.65e3ca2a.chunk.js"
+  },
+  {
+    "revision": "3dafaa7b01b8e86ab0b1",
+    "url": "/alesjia/gh-pages-sample/static/js/runtime~main.bbbea3a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/alesjia/gh-pages-sample/static/media/logo.5d5d9eef.svg"
+  }
+]);
